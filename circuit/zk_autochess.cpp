c54ec232bec4b8fb25e3251cd87a4d47d5e3e40d
Franco Nieddu <nieddu@taceo.io>
2023-11-11 22:14:34 +0300

    improved circuit

--- circuit/zk_autochess.cpp
+++ circuit/zk_autochess.cpp
@@ -1,18 +1,16 @@
 #include <nil/crypto3/algebra/curves/pallas.hpp>
 
-#define MAX_ROUND 50
 #define BOARD_SIZE 8
 #define DOES_UNIT_HIT(to_hit, player)                     \
     if (is_empty(to_hit) || is_my_unit(to_hit, player)) { \
-        did_hit = did_hit || false;                                 \
+        did_hit = did_hit || false;                       \
     } else {                                              \
         to_hit.hp -= 20;                                  \
-        did_hit = did_hit || true;                                  \
+        did_hit = did_hit || true;                        \
     }
 
 struct __attribute__((packed)) Unit {
     unsigned type;
-    // TODO move to signed
     unsigned hp;
 };
 
@@ -40,36 +38,32 @@ Unit get_unit(unsigned unit_type) {
     }
 }
 
-void transition_player_one(Board &board) {
+bool transition_player_one(Board &board) {
+    bool could_move = false;
     for (unsigned i = BOARD_SIZE; i > 0; --i) {
         for (unsigned j = BOARD_SIZE; j > 0; --j) {
             if (is_my_unit(board.val[i][j], 1) && is_empty(board.val[i + 1][j]) && i != BOARD_SIZE) {
                 board.val[i + 1][j] = board.val[i][j];
                 board.val[i][j] = empty_unit();
+                could_move = true;
             }
         }
     }
+    return could_move;
 }
 
-void transition_player_two(Board &board) {
+bool transition_player_two(Board &board) {
+    bool could_move = false;
     for (unsigned i = 1; i <= BOARD_SIZE; ++i) {
         for (unsigned j = 1; j <= BOARD_SIZE; ++j) {
             if (is_my_unit(board.val[i][j], 2) && is_empty(board.val[i - 1][j]) && i != 1) {
                 board.val[i - 1][j] = board.val[i][j];
                 board.val[i][j] = empty_unit();
+                could_move = true;
             }
         }
     }
-}
-
-void hit_unit(Unit &to_hit, unsigned player) {
-    if (!is_empty(to_hit) && !is_my_unit(to_hit, player)) {
-        if (to_hit.hp < 20) {
-            to_hit.hp = 0;
-        } else {
-            to_hit.hp -= 20;
-        }
-    }
+    return could_move;
 }
 
 bool is_dead(Unit &unit) {
@@ -86,7 +80,16 @@ void kill_dead_units(Board &board) {
     }
 }
 
-void all_units_hit(Board &board) {
+bool hit_unit(Unit &to_hit, unsigned player) {
+    if (is_empty(to_hit) || is_my_unit(to_hit, player)) {
+        return false;
+    } else {
+        to_hit.hp -= 20;
+        return true;
+    }
+}
+
+bool all_units_hit(Board &board) {
     bool did_hit = false;
     // hit player one
     for (unsigned i = 1; i <= BOARD_SIZE; ++i) {
@@ -114,6 +117,7 @@ void all_units_hit(Board &board) {
             }
         }
     }
+    return did_hit;
 }
 
 unsigned check_winner(Board &board) {
@@ -137,7 +141,9 @@ unsigned check_winner(Board &board) {
     }
 }
 
-[[circuit]] unsigned run_game(unsigned should_winner, std::array<std::array<unsigned, BOARD_SIZE>, BOARD_SIZE / 2> player_one,
+[[circuit]] unsigned run_game(unsigned should_winner,
+                              std::array<std::array<unsigned, BOARD_SIZE>, BOARD_SIZE / 2>
+                                  player_one,
                               std::array<std::array<unsigned, BOARD_SIZE>, BOARD_SIZE / 2>
                                   player_two) {
 
@@ -162,14 +168,15 @@ unsigned check_winner(Board &board) {
         }
     }
 
-    // bool another_round = true;
     // game logic
-    for (unsigned i = 0; i < MAX_ROUND; ++i) {
-        transition_player_one(board);
-        transition_player_two(board);
-        all_units_hit(board);
+    bool another_round = true;
+    while (another_round) {
+        another_round = false;
+        another_round = another_round || transition_player_one(board);
+        another_round = another_round || transition_player_two(board);
+        another_round = another_round || all_units_hit(board);
         kill_dead_units(board);
-    }
+    } 
     unsigned is_winner = check_winner(board);
     __builtin_assigner_exit_check(is_winner == should_winner);
     return is_winner;
