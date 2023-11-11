1db0411b35aed56f7dfd0f8fb7ece84ad995ea2f
koshkens <cawa.ka33@gmail.com>
2023-11-11 15:50:16 +0300

    change game tick to sync attack

--- backend-game/src/game/game.service.ts
+++ backend-game/src/game/game.service.ts
@@ -52,32 +52,37 @@ export class GameField {
     }
   }
 
-  private attackIfPossible(warrior: Warrior): boolean {
-    const enemy = this.isEnemyNearby(warrior)
-    if (enemy) {
-      enemy.health -= warrior.attack
-      console.log(`Warrior ${warrior.id} from Team ${warrior.team} attacks an enemy ${enemy.id} from Team ${enemy.team}`)
-      return true
-    } else {
-      return false
-    }
-  }
-
   public simulateTick(): boolean {
     let actionOccurred = false
 
+    // Перемещаем всех воинов
     this.warriors.forEach((warrior) => {
-      const isMove = this.moveWarrior(warrior)
-      const isAttack = this.attackIfPossible(warrior)
-
-      if (isMove || isAttack) {
+      if (this.moveWarrior(warrior)) {
         actionOccurred = true
       }
     })
 
+    // Проводим атаки после всех перемещений
+    const attacks = this.warriors.map((warrior) => this.attackIfPossible(warrior))
+    if (attacks.some((attack) => attack)) {
+      actionOccurred = true
+    }
+
+    // Проверяем состояние воинов после атак
     this.warriors = this.warriors.filter((warrior) => warrior.health > 0)
 
     return actionOccurred
+  }
+
+  private attackIfPossible(warrior: Warrior): boolean {
+    const enemy = this.isEnemyNearby(warrior)
+    if (enemy) {
+      // Применяем урон к врагу
+      enemy.health -= warrior.attack
+      // Также возвращаем true, если враг также атакует в ответ (синхронная атака)
+      return true
+    }
+    return false
   }
 
   public isGameOver(lastTickAction: boolean): boolean {
