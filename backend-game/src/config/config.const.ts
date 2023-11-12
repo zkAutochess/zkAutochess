a0a5761abb4eaf956ee7e5136edf0fd14cc12fea
koshkens <cawa.ka33@gmail.com>
2023-11-12 10:41:39 +0300

    integrate proves

--- backend-game/src/config/config.const.ts
+++ backend-game/src/config/config.const.ts
@@ -1,1 +1,44 @@
 export const DEFAULT_HTTP_PORT = 3000
+export const CONTRACT_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'bytes',
+        name: 'blob',
+        type: 'bytes',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'init_params',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'int256[][]',
+        name: 'columns_rotations',
+        type: 'int256[][]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'public_input',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'address',
+        name: 'gate_argument',
+        type: 'address',
+      },
+    ],
+    name: 'verify',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: 'result',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+]
+
+export const CONTRACT_METHOD_VERIFY = 'verify'
